613d7deb69c4929314ce17f1f0f22b1107c4a29f
Priyanka Dusija <priyankadusija@gmail.com>
2023-09-10 08:01:21 +0530

    Addimg more HTML tags and a README file.

--- App.js
+++ App.js
@@ -2,6 +2,11 @@
  * <div id="parent">
  *      <div id="child">
  *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
  *      </div>
  * </div>
  */
@@ -9,11 +14,31 @@
 const parent = React.createElement("div", {
         id: "parent"
     },
-    React.createElement("div", {
-            id: "child"
-        },
-        React.createElement("h1", {},
-            "I'm an h1 tag")));
+    [React.createElement("div", {
+                id: "child"
+            },
+            [React.createElement("h1", {
+                        id: "h1"
+                    },
+                    "I'm an h1 tag"),
+                React.createElement("h2", {
+                        id: "h2"
+                    },
+                    "I'm an h2 tag")
+            ]),
+        React.createElement("div", {
+                id: "child2"
+            },
+            [React.createElement("h1", {
+                        id: "h1_2"
+                    },
+                    "I'm an h1 tag"),
+                React.createElement("h2", {
+                        id: "h2_2"
+                    },
+                    "I'm an h2 tag")
+            ])
+    ]);
 
 console.log(parent);
 
